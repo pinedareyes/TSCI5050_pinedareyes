440cecbdb0a52d64e96f8682117cd20929af9334
Ana Pineda <analaupi@anas-air.v806.med.uthscsa.edu>
2025-01-28 11:18:44 -0600

    example class

--- R_step_by_step.R
+++ R_step_by_step.R
@@ -357,3 +357,14 @@ whatisthis(perf) # gives class of the variable
 #+ ## multiple comparison
 perf %>% tidy() %>% select(c("p.value")) %>% slice(-1)
 perf %>% tidy() %>% select(c("p.value")) %>% slice(-1) %>% unlist() %>% p.adjust()
+library(ggplot2)
+library(dplyr)
+library(nycflights13)
+
+not_cancelled <- flights %>% 
+  filter(!is.na(dep_delay), !is.na(arr_delay))
+
+not_cancelled %>% 
+  group_by(year, month, day) %>% 
+  summarise(mean = mean(dep_delay))
+
