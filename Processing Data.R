8c8a4b6013ad1ee27c244f36d1a6e44ba01fa923
AnaPineda <apineda@uthsc.edu>
2025-05-06 12:06:51 -0500

    05.06.2025-modeling

--- Processing Data.R
+++ Processing Data.R
@@ -11,9 +11,12 @@ library(rio);# simple command for importing and exporting
 library(pander); # format tables
 #library(printr); # set limit on number of lines printed
 library(broom); # allows to give clean dataset
-library(dplyr); #add dplyr library
+library(dplyr);#add dplyr library
+library(tidymodels);
+library(ggfortify);
 #init----
-options(max.print=500);
+tidymodels_prefer()
+  options(max.print=500);
 panderOptions('table.split.table',Inf); panderOptions('table.split.cells',Inf);
 probmissing=c(0.99,0.01)
 whatisthis <- function(xx){
@@ -25,10 +28,25 @@ whatisthis <- function(xx){
 
 # Import Data ----
 datafile<- "data/Simulated Data.xlsx"
-SimData<- import(datafile) %>%mutate(train=sample(TRUE,FALSE,n(),replace=TRUE))
+SimData <- import(datafile) %>%
+  mutate(train = sample(c(TRUE, FALSE), size = n(), replace = TRUE)) %>% as.data.frame()
 #SimData<- mutate(SimData, train=sample(TRUE,FALSE,n(),replace=TRUE))
 
 #Scatter Plot Matrix---- 
 select(SimData, !any_of(c("ID", "PIN", "VISIT", "Specimen ID")))[,50:52] %>% ggpairs()
-#' NOTES:Use to create a scatter plt from the data available. The number of the columns that you want to plot are inside the brackets
-c()
+#' NOTES:Use to create a scatter plot from the data available. The number of the columns that you want to plot are inside the brackets
+
+#Modeling Data
+lm_spec <- linear_reg() %>%  set_mode("regression") %>%  set_engine("lm")
+the_workflow <- workflow() %>% 
+  add_formula(`CD4/8 Ratio` ~ IHG + Glucose + BUN + Creatinine) %>% 
+  add_model(lm_spec)
+lm_fit <- fit(the_workflow,SimData)
+tidy(lm_fit)
+autoplot(lm_fit)
+lm_fit2<- the_workflow %>% update_formula(`CD4/8 Ratio` ~ Glucose *IHG + BUN*IHG + Creatinine*IHG) %>% 
+  fit(SimData)
+lm_results<- extract_fit_parsnip(lm_fit)$fit
+lm_results2<- extract_fit_parsnip(lm_fit2)$fit
+#compare models
+anova(lm_results, lm_results2)
